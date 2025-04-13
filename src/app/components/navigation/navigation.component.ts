88156f7ef9f3d3d4ca667dcd894b0f65300ab4bb
LilyDeveloppement <lilydeveloppement@gmail.com>
2025-04-13 11:53:17 +0200

    menu

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [RouterModule, MatToolbarModule, MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatMenuModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+
+}
